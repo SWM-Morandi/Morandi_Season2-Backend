67a4ca439905c19ce7998356826aef1ff4539e30
aj4941@naver.com <aj4941@naver.com>
2024-07-26 21:14:46 +0900

    :pencil2: AWS SQS Region 수정

--- src/main/java/kr/co/morandi/backend/defense_management/infrastructure/config/codesubmit/AmazonSqsConfig.java
+++ src/main/java/kr/co/morandi/backend/defense_management/infrastructure/config/codesubmit/AmazonSqsConfig.java
@@ -3,13 +3,17 @@ package kr.co.morandi.backend.defense_management.infrastructure.config.codesubmi
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Slf4j
 @Configuration
@@ -25,9 +29,24 @@ public class AmazonSqsConfig {
     private String region;
 
     @Bean
-    public AmazonSQSAsync amazonCompileSQSAws() {
+    public SqsAsyncClient sqsAsyncClient(){
+        return SqsAsyncClient
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    @Bean
+    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient){
+        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+    }
+
+    @Bean
+    public AmazonSQS amazonSQS() {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonSQSAsyncClientBuilder.standard()
+        return AmazonSQSClientBuilder.standard()
                 .withRegion(Regions.AP_NORTHEAST_2)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
